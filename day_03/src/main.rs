be82d0334cb06aef78d82f7f17d4c7da3df9818a
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2021-12-02 23:31:19 -0600

    stub in third day

---
+++ day_03/src/main.rs
@@ -0,0 +1,17 @@
+use common::read_puzzle_input;
+
+fn main() {
+    let _input_entries = read_puzzle_input(3);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const REFERENCE_INPUT: &str = "forward 5\n\
+                                   down 2";
+
+    #[test]
+    fn test_empty() {
+    }
+}
