9a52d9254958534c74dcdd9c016ad6384373a9a7
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2021-12-05 14:01:49 -0600

    solve part one day 4

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -9,6 +9,18 @@ struct Board {
 
 impl Board {
     fn is_solved(&self) -> bool {
+        // Check rows
+        if self.numbers.chunks(5).any(|row| row.iter().all(|(_, mark)| *mark)) {
+            return true;
+        }
+
+        // Check columns
+        for column in 0..5 {
+            if self.numbers.chunks(5).all(|row| row[column].1) {
+                return true;
+            }
+        }
+
         false
     }
 
@@ -53,6 +65,9 @@ fn main() {
     for board in input[1..].chunks(6) {
         board_list.push(parse_board(board));
     }
+
+    let result = play_boards(&mut board_list, &rng_nums);
+    println!("first part result: {:?}", result);
 }
 
 fn parse_board(lines: &[String]) -> Board {
@@ -64,6 +79,20 @@ fn parse_random_header(line: &str) -> Vec<usize> {
         .split(',')
         .map(|num| num.parse::<usize>().unwrap())
         .collect()
+}
+
+fn play_boards(boards: &mut Vec<Board>, call_outs: &Vec<usize>) -> Option<usize> {
+    for number in call_outs.iter() {
+        for board in boards.iter_mut() {
+            board.mark_number(*number);
+
+            if board.is_solved() {
+                return Some(*number * board.unmarked_score());
+            }
+        }
+    }
+
+    None
 }
 
 #[cfg(test)]
@@ -99,6 +128,20 @@ mod tests {
             rng_nums[..12],
             vec![7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24]
         );
+    }
+
+    #[test]
+    fn test_reference_game() {
+        let input: Vec<String> = REFERENCE_INPUT.lines().map(|e| e.to_string()).collect();
+
+        let rng_nums = parse_random_header(&input[0]);
+
+        let mut board_list = vec![];
+        for board in input[1..].chunks(6) {
+            board_list.push(parse_board(board));
+        }
+
+        assert_eq!(play_boards(&mut board_list, &rng_nums), Some(4512));
     }
 
     #[test]
